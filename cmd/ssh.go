8eeadcabc64661d948f74bb5accf9c870b74b2b1
Jimmy Cann <mail@jimmycann.com>
2018-05-26 18:20:53 +1000

    Add `tok ssh` command

---
+++ cmd/ssh.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"bitbucket.org/ironstar/tokaido-cli/services/drupal"
+	"bitbucket.org/ironstar/tokaido-cli/utils"
+
+	"github.com/spf13/cobra"
+)
+
+// SSHCmd - `tok status`
+var SSHCmd = &cobra.Command{
+	Use:   "ssh",
+	Short: "SSH into your Drush container",
+	Long:  "SSH into your Drush container using the Tokaido generated key",
+	Run: func(cmd *cobra.Command, args []string) {
+		utils.CheckCmdHard("docker-compose")
+
+		drupal.DrushSSH()
+	},
+}
