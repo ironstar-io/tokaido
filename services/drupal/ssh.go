866e2eced09dfadf1f18d5804f88055cda6fd81f
Jimmy Cann <mail@jimmycann.com>
2018-05-27 18:19:24 +1000

    Loose ssh implementation tests

--- services/drupal/ssh.go
+++ services/drupal/ssh.go
@@ -3,13 +3,19 @@ package drupal
 import (
 	"bitbucket.org/ironstar/tokaido-cli/services/docker"
 	"bitbucket.org/ironstar/tokaido-cli/system/fs"
-	"bitbucket.org/ironstar/tokaido-cli/utils"
+	"bitbucket.org/ironstar/tokaido-cli/system/ssh"
+
+	"log"
 )
 
 // DrushSSH SSH into the Drush container
 func DrushSSH() {
 	drushPort := docker.LocalPort("drush", "22")
-	sshString := `ssh tok@localhost -p ` + drushPort + ` -i ` + fs.HomeDir() + `/.ssh/tok_ssh.key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null`
 
-	utils.BashStringCmd(sshString)
+	client, err := ssh.DialWithKey("localhost:"+drushPort, "tok", fs.HomeDir()+"/.ssh/tok_ssh.key")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer client.Close()
 }
