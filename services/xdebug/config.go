778acfdd0ae8a9f340579ff278f739eeb6b9e23c
Jimmy Cann <mail@jimmycann.com>
2018-06-23 12:27:17 +1000

    Rebuild compose.tok with xdebug gateway values

--- services/xdebug/config.go
+++ services/xdebug/config.go
@@ -2,9 +2,16 @@ package xdebug
 
 import (
 	"bitbucket.org/ironstar/tokaido-cli/services/docker"
+	"bitbucket.org/ironstar/tokaido-cli/system/fs"
 
+	"bufio"
+	"bytes"
 	"fmt"
+	"os"
+	"strings"
 )
+
+var tokComposePath = fs.WorkDir() + "/docker-compose.tok.yml"
 
 // Configure - Amend docker-compose.tok.yml to be compatible with xdebug
 func Configure() {
@@ -22,9 +29,27 @@ func regenerateGateway(networkUp bool) {
 	}
 
 	networkGateway := docker.GetGateway()
-	fmt.Println(networkGateway)
 
-	//TODO Regen compose.tok.yml with gateway IP set
+	f, openErr := os.Open(tokComposePath)
+	if openErr != nil {
+		fmt.Println(openErr)
+		return
+	}
+
+	defer f.Close()
+
+	closePHP := "XDEBUG_REMOTE_HOST: "
+	var buffer bytes.Buffer
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), closePHP) {
+			buffer.Write([]byte("      XDEBUG_REMOTE_HOST: " + networkGateway + "\n"))
+		} else {
+			buffer.Write([]byte(scanner.Text() + "\n"))
+		}
+	}
+
+	fs.Replace(tokComposePath, buffer.Bytes())
 
 	docker.KillContainer("fpm")
 	docker.UpContainer("fpm")
--- utils/stdout_cmd.go
+++ utils/stdout_cmd.go
@@ -27,7 +27,6 @@ func StdoutCmd(name string, args ...string) string {
 	return strings.TrimSpace(string(stdoutStderr))
 }
 
-<<<<<<< HEAD
 // StdoutStreamCmd - Execute a command on the users' OS and stream stdout
 func StdoutStreamCmd(name string, args ...string) {
 	DebugCmd(name + " " + strings.Join(args, " "))
@@ -36,46 +35,4 @@ func StdoutStreamCmd(name string, args ...string) {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Run()
-=======
-// CommandSubSplitOutput - Execute a command and return the output value split into stdout and stderr. No exit on stdErr
-func CommandSubSplitOutput(name string, args ...string) (string, error) {
-	cmd := exec.Command(name, args...)
-	cmd.Dir = fs.WorkDir()
-	stdoutStderr, err := cmd.CombinedOutput()
-	if err != nil {
-		return "", err
-	}
-
-	return strings.TrimSpace(string(stdoutStderr)), nil
-}
-
-// SilentBashStringCmd - Execute a bash command from a string `bash -c "(cmd)" with no log output`
-func SilentBashStringCmd(cmdStr string) string {
-	cmd := exec.Command("bash", "-c", cmdStr)
-	cmd.Dir = fs.WorkDir()
-	stdoutStderr, _ := cmd.CombinedOutput()
-
-	return strings.TrimSpace(string(stdoutStderr))
-}
-
-// BashStringSplitOutput - Execute a bash command splitting the resulting stdout and stderr
-func BashStringSplitOutput(cmdStr string) (string, error) {
-	cmd := exec.Command("bash", "-c", cmdStr)
-	cmd.Dir = fs.WorkDir()
-	stdoutStderr, err := cmd.CombinedOutput()
-	if err != nil {
-		return "", err
-	}
-
-	return strings.TrimSpace(string(stdoutStderr)), nil
-}
-
-// BashStringCmd - Execute a bash command from a string `bash -c "(cmd)"`
-func BashStringCmd(cmdStr string) string {
-	stdoutStderr := SilentBashStringCmd(cmdStr)
-
-	fmt.Printf("%s\n", stdoutStderr)
-
-	return string(stdoutStderr)
->>>>>>> Begin build out for debug configuration
 }
