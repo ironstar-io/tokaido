b344c89146b145ebf806e76f6c913fac820c22f2
Mike Richardson <hello@mikerichardson.com.au>
2018-07-01 17:46:56 +1000

    Initial mac testing

--- system/osx/sync.go
+++ system/osx/sync.go
@@ -1,21 +1,134 @@
 package osx
 
-// RegisterLaunchdService Register the unison sync service for systemd
-func RegisterLaunchdService() bool {
-	return true
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"os"
+
+	"bitbucket.org/ironstar/tokaido-cli/conf"
+	"bitbucket.org/ironstar/tokaido-cli/system/osx/templates"
+	"bitbucket.org/ironstar/tokaido-cli/utils"
+)
+
+type service struct {
+	ProjectName string
+	ProjectPath string
 }
 
-// StartLaunchdService ...
-func StartLaunchdService() bool {
-	return true
+type status struct {
+	PID int64
+}
+
+func createSyncFile() {
+	c := conf.GetConfig()
+
+	s := service{
+		ProjectName: c.Project,
+		ProjectPath: c.Path,
+	}
+
+	serviceFilename := "tokaido.sync." + s.ProjectName
+
+	tmpl := template.New(serviceFilename)
+	tmpl, err := tmpl.Parse(synctmpl.SyncTemplateStr)
+
+	if err != nil {
+		log.Fatal("Parse: ", err)
+		return
+	}
+
+	var tpl bytes.Buffer
+	if err := tmpl.Execute(&tpl, s); err != nil {
+		log.Fatal("Parse: ", err)
+		return
+	}
+
+	writeSyncFile(tpl.String(), c.LaunchdPath, serviceFilename)
+}
+
+func writeSyncFile(body string, path string, filename string) {
+	mkdErr := os.MkdirAll(path, os.ModePerm)
+	if mkdErr != nil {
+		log.Fatal("Mkdir: ", mkdErr)
+	}
+
+	var file, err = os.Create(path + filename)
+	if err != nil {
+		log.Fatal("Create: ", err)
+	}
+
+	_, _ = file.WriteString(body)
+
+	defer file.Close()
+}
+
+func startSyncService() {
+	c := conf.GetConfig()
+	_, err := utils.CommandSubSplitOutput("launchctl", "start", "tokaido.sync."+c.Project+".plist")
+	if err != nil {
+		log.Fatal("Unable to start sync service: ", err)
+	}
+}
+
+func stopSyncService() {
+	c := conf.GetConfig()
+	_, err := utils.CommandSubSplitOutput("launchctl", "stop", "tokaido.sync."+c.Project+".plist")
+	if err != nil {
+		log.Fatal("Unable to start sync service: ", err)
+	}
+}
+
+func deleteSyncService() {
+	c := conf.GetConfig()
+	rmErr := os.Remove(c.LaunchdPath + "/tokaido.sync." + c.Project + ".plist")
+	if rmErr != nil {
+		log.Fatal("Unable to start sync service: ", rmErr)
+	}
+}
+
+// RegisterLaunchdService Register the unison sync service for launchd
+func RegisterLaunchdService() {
+	fmt.Println(`
+🔄  Creating a background process to sync your local repo into the Tokaido environment
+	`)
+	createSyncFile()
+}
+
+// StartLaunchdService Start the launchd service after it is created
+func StartLaunchdService() {
+	startSyncService()
+	CheckSyncService()
+}
+
+// CheckSyncService checks if the unison background process is running
+func CheckSyncService() error {
+	c := conf.GetConfig()
+	s, err := utils.CommandSubSplitOutput("launchctl", "list", "tokaido.sync."+c.Project+".plist")
+	if err != nil {
+		return err
+	}
+
+	// Marhsal the result into JSON so we can analyse the pid number.
+	// If launchd returns a pid, that's how we know a service is running
+	var j status
+	jsonErr := json.Unmarshal([]byte(s), &j)
+	if jsonErr != nil {
+		return err
+	}
+	fmt.Printf("PID = %d", j.PID)
+
+	return nil
+
 }
 
 // StopLaunchdService ...
-func StopLaunchdService() bool {
-	return true
-}
-
-// CheckSyncService ...
-func CheckSyncService() bool {
-	return true
+func StopLaunchdService() {
+	fmt.Println(`
+🔄  Removing the background sync process
+	`)
+	stopSyncService()
+	deleteSyncService()
 }
