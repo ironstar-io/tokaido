69629eeac3ee8cb2fe8bcac34e8fca9dd3b9ea51
Mike Richardson <hello@mikerichardson.com.au>
2018-07-08 09:11:44 +1000

    merge in tok init output

--- services/tok/init.go
+++ services/tok/init.go
@@ -48,15 +48,11 @@ func Init() {
 
 	fmt.Println()
 
-<<<<<<< HEAD
 	if docker.ImageExists("tokaido/drush:latest") == false {
 		fmt.Println(`🚡  First time lifting your containers? There's a few images to download, this might take some time.`)
 	}
 
 	wo := wow.New(os.Stdout, spin.Get(spin.Dots), `   Tokaido is pulling up your containers!`)
-=======
-	wo := wow.New(os.Stdout, spin.Get(spin.Dots), `   Tokaido is starting your containers`)
->>>>>>> master
 	wo.Start()
 
 	docker.Up()
