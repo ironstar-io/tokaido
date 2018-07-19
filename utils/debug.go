c669318a8105577040bb3e1940ef0e43448b0005
Jimmy Cann <mail@jimmycann.com>
2018-07-19 17:40:38 +1000

    Debug output with a colours library to avoid extraneous character output in Powershell

--- utils/debug.go
+++ utils/debug.go
@@ -3,33 +3,33 @@ package utils
 import (
 	"bitbucket.org/ironstar/tokaido-cli/conf"
 
-	"fmt"
+	"github.com/fatih/color"
 )
 
 // DebugCmd ...
 func DebugCmd(cmd string) {
 	if conf.GetConfig().Debug == true && cmd != "" {
-		fmt.Printf("\033[33mRunning command: '%s'\033[0m\n", cmd)
+		color.Yellow("Running command: '%s'", cmd)
 	}
 }
 
 // DebugString ...
 func DebugString(output string) {
 	if conf.GetConfig().Debug == true && len(output) > 0 {
-		fmt.Printf("\033[33m%s\033[0m\n", output)
+		color.Yellow("%s", output)
 	}
 }
 
 // DebugOutput ...
 func DebugOutput(output []byte) {
 	if conf.GetConfig().Debug == true && len(output) > 0 {
-		fmt.Printf("\033[33m%s\033[0m\n", output)
+		color.Yellow("%s", output)
 	}
 }
 
 // DebugErrOutput ...
 func DebugErrOutput(output error) {
 	if conf.GetConfig().Debug == true {
-		fmt.Printf("\033[31m%s\033[0m\n", output)
+		color.Red("%s", output)
 	}
 }
