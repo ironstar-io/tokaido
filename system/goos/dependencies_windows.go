7d8e5434f1e8ff46a4a539bad42cedd51af84e9e
Jimmy Cann <mail@jimmycann.com>
2018-07-20 23:08:55 +1000

    Automate unison installation in dependency check step

--- system/goos/dependencies_windows.go
+++ system/goos/dependencies_windows.go
@@ -1,10 +1,15 @@
 package goos
 
 import (
+	"bitbucket.org/ironstar/tokaido-cli/system/fs"
 	"bitbucket.org/ironstar/tokaido-cli/utils"
 
 	"fmt"
+	"io"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 // CheckDependencies - Root executable
@@ -24,15 +29,34 @@ func checkChoco() {
 }
 
 // checkUnison ...
-func checkUnison() *string {
+func checkUnison() {
 	_, err := exec.LookPath("unison")
 	if err != nil {
 		fmt.Println("Unison isn't installed. Tokaido will install it with Chocolately")
-		utils.StdoutCmd("choco", "install", "unison")
-		fmt.Println("Unison installed successfully")
-	}
 
-	return nil
+		utils.StdoutCmd("choco", "install", "unison")
+
+		// Unison doesn't install as unison.exe in the chocolatey bin, this copies the installed binary so it's accessible from your PATH
+		var ue string
+		cbin := filepath.Join(os.Getenv("SYSTEMDRIVE")+"/", "ProgramData", "chocolatey", "bin")
+		err := filepath.Walk(cbin, func(path string, info os.FileInfo, err error) error {
+			if info.IsDir() {
+				return nil
+			}
+			if strings.Contains(path, "unison") && strings.Contains(path, "text") {
+				ue = path
+				return io.EOF
+			}
+			return nil
+		})
+
+		if err == io.EOF {
+			fs.Copy(ue, filepath.Join(cbin, "unison.exe"))
+			fmt.Println("Unison installed successfully")
+		} else {
+			fmt.Println("Unison may not have installed on your system correctly, this may cause issues with Tokaido initialization")
+		}
+	}
 }
 
 // CheckAndChocoInstall ...
