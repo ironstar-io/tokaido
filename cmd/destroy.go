d69eae9933bfc2a11ff50455992d710e63d1ce61
Jimmy Cann <mail@jimmycann.com>
2018-07-23 18:14:38 +1000

    Ensure background sync service doesn’t get destroyed when User selects “No” at the prompt

--- cmd/destroy.go
+++ cmd/destroy.go
@@ -4,7 +4,10 @@ import (
 	"bitbucket.org/ironstar/tokaido-cli/conf"
 	"bitbucket.org/ironstar/tokaido-cli/services/docker"
 	"bitbucket.org/ironstar/tokaido-cli/services/unison"
+	"bitbucket.org/ironstar/tokaido-cli/system/console"
 	"bitbucket.org/ironstar/tokaido-cli/utils"
+
+	"fmt"
 
 	"github.com/spf13/cobra"
 )
@@ -20,6 +23,13 @@ var DestroyCmd = &cobra.Command{
 		docker.HardCheckTokCompose()
 
 		conf.LoadConfig(cmd)
+
+		confirmDestroy := utils.ConfirmationPrompt(`🔥  This will also destroy the database inside your Tokaido environment. Are you sure?`, "n")
+		if confirmDestroy == false {
+			console.Println(`🍵  Exiting without change`, "")
+			return
+		}
+		fmt.Println()
 
 		docker.Down()
 
