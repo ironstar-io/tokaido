6acad2bb1f73158cafc3a6f2c19396d7b447b589
otakumike <otakumike@users.noreply.github.com>
2018-08-05 18:16:11 +1000

    Remove vexatious yet superfluous replaceDrupalVars func that was no longer required

--- conf/replace.go
+++ conf/replace.go
@@ -2,21 +2,28 @@ package conf
 
 import (
 	"fmt"
+	"os/exec"
+	"path/filepath"
+	"strings"
 
 	"github.com/ironstar-io/tokaido/system/fs"
-
-	"io/ioutil"
-	"log"
-	"path/filepath"
-
 	"github.com/spf13/viper"
-	"gopkg.in/yaml.v2"
 )
 
 func drupalVars(path, version string) []byte {
 	return []byte(`drupal:
   path: ` + path + `
   majorVersion: ` + version)
+}
+
+func commandSubstitution(name string, args ...string) {
+
+	cmd := exec.Command(name, args...)
+	cmd.Dir = fs.WorkDir()
+	stdoutStderr, _ := cmd.CombinedOutput()
+
+	fmt.Println(strings.TrimSpace(string(stdoutStderr)))
+
 }
 
 // CreateOrReplaceDrupalConfig ...
@@ -26,38 +33,5 @@ func CreateOrReplaceDrupalConfig(path, version string) {
 	cf := viper.ConfigFileUsed()
 	if cf == "" {
 		fs.TouchByteArray(filepath.Join(fs.WorkDir(), "/.tok/config.yml"), drupalVars(path, version))
-		return
-	}
-
-	replaceDrupalVars(cf, path, version)
-}
-
-// replaceDrupalVars ...
-func replaceDrupalVars(cf, path, version string) {
-	confStruct := Config{}
-	confStruct.System.Syncsvc.Enabled = true
-
-	iocf, err := ioutil.ReadFile(cf)
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-
-	errTwo := yaml.Unmarshal(iocf, &confStruct)
-	if errTwo != nil {
-		log.Fatalf("error: %v", errTwo)
-	}
-
-	confStruct.Drupal.Path = path
-	confStruct.Drupal.Majorversion = version
-
-	confYml, err := yaml.Marshal(&confStruct)
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-
-	fs.Replace(cf, confYml)
-
-	if err := viper.ReadInConfig(); err != nil {
-		fmt.Printf(`There was an error reading your config file. This command may need to be run again.`)
 	}
 }
