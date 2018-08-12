53aa125288399b8a7c5de39c2211d17f3fbac0a3
otakumike <otakumike@users.noreply.github.com>
2018-08-12 17:46:35 +1000

    refactor dep check for linux

--- system/goos/dependencies_linux.go
+++ system/goos/dependencies_linux.go
@@ -1,70 +1,47 @@
 package goos
 
 import (
+	"fmt"
 	"log"
 	"os/exec"
-
-	"github.com/ironstar-io/tokaido/system/console"
 )
 
-// CheckDependencies - Root executable
-func CheckDependencies() *string {
-	errDocker := CheckDocker()
-	errDockerCompose := CheckDockerCompose()
-	errUnison := CheckUnison()
-	errUnisonFsmonitor := CheckUnisonFsmonitor()
+// commandCheck looks for the presence of command named 'n' on the system
+// it returns true or false based on whether or not the command is available
+func commandCheck(n string) bool {
+	_, err := exec.LookPath(n)
+	if err != nil {
+		return false
+	}
 
-	if (errDocker != nil) ||
-		(errDockerCompose != nil) ||
-		(errUnison != nil) ||
-		(errUnisonFsmonitor != nil) {
+	return true
+}
+
+// CheckDependencies - Root executable
+func CheckDependencies() {
+	d := map[string]bool{
+		"docker":           false,
+		"docker-compose":   false,
+		"unison":           false,
+		"unison-fsmonitor": false,
+	}
+
+	m := map[string]string{
+		"docker":           "😓  docker is missing. You need to install Docker CE or Docker Machine",
+		"docker-compose":   "😓  docker-compose is missing. Please visit https://docs.docker.com/compose/install/ for install instructions",
+		"unison":           "😓  unison is missing. Please follow the install instructions at https://tokaido.io/docs/getting-started/#installing-on-linux",
+		"unison-fsmonitor": "😓  unison-fsmonitor is missing. Please follow the install instructions at https://tokaido.io/docs/getting-started/#installing-on-linux",
+	}
+
+	var f bool
+	for k := range d {
+		if !commandCheck(k) {
+			fmt.Println(m[k])
+			f = true
+		}
+	}
+
+	if f {
 		log.Fatalf("Unable to proceed. One or more dependency checks failed")
 	}
-
-	return nil
-}
-
-// CheckDocker ...
-func CheckDocker() error {
-	_, err := exec.LookPath("docker")
-	if err != nil {
-		console.Println(`😓  docker is missing. You need to install Docker CE or Docker Machine`, "×")
-		return err
-	}
-
-	return nil
-}
-
-// CheckDockerCompose ...
-func CheckDockerCompose() error {
-	_, err := exec.LookPath("docker-compose")
-	if err != nil {
-		console.Println(`😓  docker-compose is missing. Please visit https://docs.docker.com/compose/install/ for install instructions`, "x")
-		return err
-	}
-
-	return nil
-}
-
-// CheckUnison ...
-func CheckUnison() error {
-	_, err := exec.LookPath("unison")
-	if err != nil {
-		console.Println(`😓  unison is missing. Please follow the install instructions at https://tokaido.io/docs/getting-started/#installing-on-linux`, "x")
-		return err
-	}
-
-	return nil
-}
-
-// CheckUnisonFsmonitor ...
-func CheckUnisonFsmonitor() error {
-	_, err := exec.LookPath("unison-fsmonitor")
-	if err != nil {
-		console.Println(`😓  unison-fsmonitor is missing. Please follow the install instructions at https://tokaido.io/docs/getting-started/#installing-on-linux`, "x")
-
-		return err
-	}
-
-	return nil
 }
