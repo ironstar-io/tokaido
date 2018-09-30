cfeae7a10e85f5f8f2c916b5c8cc89249918ab5c
otakumike <otakumike@users.noreply.github.com>
2018-09-30 14:10:02 +1000

    tweak messaging

--- services/tok/goos/init_unix.go
+++ services/tok/goos/init_unix.go
@@ -21,7 +21,7 @@ Your Drupal development environment is now up and running
 	console.Println(`💻  Run "ssh `+conf.GetConfig().Tokaido.Project.Name+`.tok" to ssh into the environment`, "-")
 	console.Println(`🌎  Run "tok open" to open the environment in your browser`, "-")
 	console.Println(`👀  Run "tok exec" to run one-time commands like 'tok exec drush status'`, "-")
-	console.Println(`🤔  Run "tok status" check the status of your environment`, "-")
+	console.Println(`🤔  Run "tok status" to check the status of your environment`, "-")
 	fmt.Println(`
 Check out https://tokaido.io/docs/using/ for tips to help you get the most out of your Tokaido environment
 	`)
--- services/tok/goos/init_windows.go
+++ services/tok/goos/init_windows.go
@@ -22,7 +22,7 @@ Your Drupal development environment is now up and running
 	console.Println(`💻  Run "ssh `+conf.GetConfig().Tokaido.Project.Name+`.tok" to ssh into the environment`, "-")
 	console.Println(`👀  Run "tok exec" to run commands like 'tok exec drush status'`, "-")
 	console.Println(`🌎  Run "tok open" to open the environment in your browser`, "-")
-	console.Println(`🚀  Run "tok status" check the status of your environment`, "-")
+	console.Println(`🚀  Run "tok status" to check the status of your environment`, "-")
 	fmt.Println(`
 Check out https://tokaido.io/docs/using/ for tips to help you get the most out of your Tokaido environment
 	`)
