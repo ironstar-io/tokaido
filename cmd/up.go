a8dde731f430d480f505a3919025d314f013bc48
Jimmy Cann <JCann@officeworks.com.au>
2018-10-14 23:52:26 +1100

    Begin defining a pattern for having some commands avoid hard project root check

--- cmd/up.go
+++ cmd/up.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"github.com/ironstar-io/tokaido/conf"
 	"github.com/ironstar-io/tokaido/services/tok"
 	"github.com/ironstar-io/tokaido/utils"
+	"github.com/ironstar-io/tokaido/constants"
+
+	"log"
 
 	"github.com/spf13/cobra"
 )
@@ -14,6 +18,9 @@ var UpCmd = &cobra.Command{
 	Long:  "Start a Tokaido local development environment",
 	Run: func(cmd *cobra.Command, args []string) {
 		utils.CheckCmdHard("docker-compose")
+		if conf.GetConfig().Tokaido.Project.Name == constants.ProjectRootNotFound {
+			log.Fatal(constants.ProjectNotFoundError)
+		}
 
 		tok.Init()
 		tok.InitMessage()
