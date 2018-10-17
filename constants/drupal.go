2d305e02b760e0ce6ab402f3d528f122eb3f6a0b
Jimmy Cann <JCann@officeworks.com.au>
2018-10-17 22:04:12 +1100

    Update const to reflect path format change

--- constants/drupal.go
+++ constants/drupal.go
@@ -1,7 +1,7 @@
 package constants
 
 // DefaultDrupalPrivatePath - To fill file_private_path
-const DefaultDrupalPrivatePath = "/tokaido/site/private/default"
+const DefaultDrupalPrivatePath = "/site/private/default"
 
 // DefaultDrupalTemporaryPath - To fill file_temporary_path
 const DefaultDrupalTemporaryPath = "/tmp"
