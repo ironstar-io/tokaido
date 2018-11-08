9f7e32ab5212fcd5a29e4ff08d8b8b8257d224f0
otakumike <otakumike@users.noreply.github.com>
2018-11-08 18:31:03 +1100

    add purge support

---
+++ services/drupal/purge.go
@@ -0,0 +1,45 @@
+package drupal
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/ironstar-io/tokaido/services/docker"
+	"github.com/ironstar-io/tokaido/system/ssh"
+)
+
+// Purge will empty the Varnish cache and run a drush cache-rebuild
+func Purge() {
+	purgeVarnish()
+	purgeDrush()
+}
+
+func purgeVarnish() {
+	port := docker.LocalPort("varnish", "8081")
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", "http://localhost:"+port, nil)
+	if err != nil {
+		log.Fatalf("Error when opening HTTP client for Varnish purge: %v", err)
+	}
+
+	req.Header.Add("X-PURGEALL", "")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Error while trying to send Varnish purge request: %v", err)
+	}
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Successfully purged Varnish cache")
+	} else {
+		log.Fatalf("Sorry, Varnish purge failed. Response: %s", resp.Status)
+	}
+}
+
+func purgeDrush() {
+	cmd := []string{"drush cache-rebuild -y"}
+	fmt.Println("Sending drush cache-rebuild request...")
+	resp := ssh.ConnectCommand(cmd)
+	fmt.Println(resp)
+}
