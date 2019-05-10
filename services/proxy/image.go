32cb1ec9e19a0a01fffe88f5f7e25759ecb71d21
Mike Richardson <otakumike@users.noreply.github.com>
2019-05-10 17:34:47 +1000

    fix bug to stop proxy pulling images when it hasn't been configured yet

--- services/proxy/image.go
+++ services/proxy/image.go
@@ -1,6 +1,25 @@
 package proxy
 
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	homedir "github.com/mitchellh/go-homedir"
+)
+
 // PullImages - Pull all images in compose file
 func PullImages() {
+	// Don't pull if the proxy config hasn't been created yet
+	h, err := homedir.Dir()
+	if err != nil {
+		log.Fatalln("Unable to resolve home directory so can't initialise Tokaido. Sorry!")
+	}
+	gc := filepath.Join(h, ".tok/proxy/docker-compose.yml")
+	_, err = os.Stat(gc)
+	if err != nil {
+		return
+	}
+
 	ComposeStdout("pull")
 }
