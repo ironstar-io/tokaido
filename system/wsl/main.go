13fe91d925bbdbdbdf56af14fe78687e7f9a59e7
Jimmy Cann <mail@jimmycann.com>
2019-10-21 17:18:30 +0900

    Add check whether user is using WSL

---
+++ system/wsl/main.go
@@ -0,0 +1,23 @@
+package wsl
+
+import (
+	"strings"
+	"io/ioutil"
+	
+	"github.com/ironstar-io/tokaido/system/fs"
+)
+
+// IsWSL - Return true if OS is using Windows Subsystem for Linux (WSL)
+func IsWSL() bool {
+	if fs.CheckExists("/proc/version") == false {
+		return false
+	}
+
+	b, err := ioutil.ReadFile("/proc/version")
+    if err != nil {
+		return false
+    }
+	s := string(b)
+		
+	return strings.Contains(s, "Microsoft")
+}
