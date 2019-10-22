907b57056f5ee0780fd54d5d497bcd945015dbdc
Jimmy Cann <mail@jimmycann.com>
2019-10-22 21:28:51 +0900

    Fix `tok open` for WSL

--- system/goos/opensite_linux.go
+++ system/goos/opensite_linux.go
@@ -2,9 +2,14 @@ package goos
 
 import (
 	"github.com/ironstar-io/tokaido/utils"
+	"github.com/ironstar-io/tokaido/system/wsl"
 )
 
 // OpenSite - Linux Root executable
 func OpenSite(url string) {
+	if wsl.IsWSL() {
+		utils.CommandSubstitution("powershell.exe", "/c", "start", url)
+	}
+
 	utils.CommandSubstitution("xdg-open", url)
 }
