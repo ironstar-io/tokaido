4aaed6dba8e2a7022a96b26e632f0d83c9773bbd
Mike Richardson <hello@mikerichardson.com.au>
2019-11-17 15:40:17 +1100

    warn

--- system/dependencies.go
+++ system/dependencies.go
@@ -1,13 +1,23 @@
 package system
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/ironstar-io/tokaido/conf"
 	"github.com/ironstar-io/tokaido/system/goos"
+
+	aurora "github.com/logrusorgru/aurora"
 )
 
 // CheckDependencies - Root executable
 func CheckDependencies() {
 	if conf.GetConfig().Tokaido.Dependencychecks == true {
 		goos.CheckDependencies()
+	}
+
+	term := os.Getenv("TERM")
+	if term != "screen-256color" && term != "xterm-256color" {
+		fmt.Println(aurora.Yellow("Warning: The terminal '" + term + "' is not supported. Please switch to screen-256color or xterm-256color"))
 	}
 }
