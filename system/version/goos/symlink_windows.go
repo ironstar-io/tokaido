65a7474bbc1d399eb602d030ee2f498f8843233d
Jimmy Cann <mail@jimmycann.com>
2019-11-20 21:47:39 +0900

    Windows symlink: try golang method followed by native command fallback

--- system/version/goos/symlink_windows.go
+++ system/version/goos/symlink_windows.go
@@ -12,12 +12,7 @@ import (
 
 // CreateSymlink - Check if a file/folder is writable
 func CreateSymlink(path string) error {
-	// Depending on OS, figure out the correct tok path and create symlink
-	var tokBinPath = filepath.Join(fs.HomeDir(), "bin", "tok")
-	tbp := strings.ReplaceAll(tokBinPath, "C:\\", "/c/")
-	t := strings.ReplaceAll(tbp, "\\", "/")
-	pa := strings.ReplaceAll(path, "C:\\", "/c/")
-	p := strings.ReplaceAll(pa, "\\", "/")
+	tokBinPath := filepath.Join(fs.HomeDir(), "bin", "tok")
 
 	if fs.CheckExists(tokBinPath) == true {
 		// Remove any existing soft link
@@ -27,14 +22,23 @@ func CreateSymlink(path string) error {
 		}
 	}
 
+	// Create a new symbolic or "soft" link
 	// os.Symlink requires elevated permissions in Windows, we can avoid that using `ln -s`. https://github.com/golang/go/issues/22874
-	// This may be flimsy on some systems
-	_, err := utils.CommandSubSplitOutput("ln", "-s", p, t)
+	err := os.Symlink(path, tokBinPath)
 	if err != nil {
-		fmt.Println("Unable to create a symlink when installing Tokaido.")
-		fmt.Println("You may need to manually add a symbolic link or directly add the Tokaido binary to your PATH\n")
-
-		return err
+		// As a fallback, this may be flimsy on some systems
+		tbp := strings.ReplaceAll(tokBinPath, "C:\\", "/c/")
+		t := strings.ReplaceAll(tbp, "\\", "/")
+		pa := strings.ReplaceAll(path, "C:\\", "/c/")
+		p := strings.ReplaceAll(pa, "\\", "/")
+	
+		_, err := utils.CommandSubSplitOutput("ln", "-s", p, t)
+		if err != nil {
+			fmt.Println("Unable to create a symlink when installing Tokaido.")
+			fmt.Println("You may need to manually add a symbolic link or directly add the Tokaido binary to your PATH\n")
+			
+			return err
+		}
 	}
 
 	return nil
