360f7418ae815b22c9c7e33647219b6b7274369b
Jimmy Cann <mail@jimmycann.com>
2019-11-21 06:57:44 +0900

    Remove `mklink` fallback

--- system/version/goos/symlink_windows.go
+++ system/version/goos/symlink_windows.go
@@ -33,24 +33,20 @@ func CreateSymlink(path string) error {
 	err = os.Symlink(path, tokBinPath)
 	if err != nil {
 		// As a fallback, this may be flimsy on some systems
-		utils.DebugString("The golang native method failed to create a symbolic link. Usually this is due to symlink creation requiring elevation. Falling back to attempt `mklink /d`")
+		utils.DebugString("The Go native method failed to create a symbolic link. Usually this is due to symlink creation requiring elevation. Falling back to attempt `mklink /d`")
 		utils.DebugErrOutput(err)
-		
-		_, err := utils.CommandSubSplitOutput("mklink", "/d", symlinkPath, executablePath)
+
+		_, err := utils.CommandSubSplitOutput("ln", "-s", executablePath, symlinkPath)
 		if err != nil {
-			utils.DebugString("The `mklink` command failed to create a symbolic link. Falling back to attempt `ln -s`")
+			fmt.Println("Unable to create a symlink when installing Tokaido.")
+			fmt.Println("You may need to manually add a symbolic link or directly add the Tokaido binary to your PATH\n")
+			
+			utils.DebugString("The `ln -s` command to create a symbolic link has also failed.")
 			utils.DebugErrOutput(err)
 
-			_, err := utils.CommandSubSplitOutput("ln", "-s", executablePath, symlinkPath)
-			if err != nil {
-				fmt.Println("Unable to create a symlink when installing Tokaido.")
-				fmt.Println("You may need to manually add a symbolic link or directly add the Tokaido binary to your PATH\n")
-				
-				utils.DebugString("The `ln -s` command to create a symbolic link has also failed.")
-
-				return err
-			}
+			return err
 		}
+
 	}
 
 	return nil
