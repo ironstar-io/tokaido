9d6fedc9d8b642cea1fd7a72294b00d14c84c83a
Mike Richardson <michael.richardson@ironstar.io>
2022-06-09 11:29:18 +1000

    more github actions testing

--- images/arm64/ssh/start.sh
+++ images/arm64/ssh/start.sh
@@ -18,9 +18,7 @@ echo "Adding your local SSH key to the 'tok' user"
 cp /app/site/.tok/local/ssh_key.pub /home/app/.ssh/authorized_keys
 
 # Set up environment variables for the user
-echo "Setting up environment variables for $username"
 echo "PATH=$PATH:/usr/local/bin" > /home/app/.ssh/environment
-
 echo "APP_ENV=${APP_ENV:-unknown}" >> /home/app/.ssh/environment
 echo "PROJECT_NAME=${PROJECT_NAME:-}" >> /home/app/.ssh/environment
 echo "DRUPAL_ROOT=${drupal_root}" >> /home/app/.ssh/environment
